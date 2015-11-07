ca3b16d986f61c4b4a3fed43316ee5316ae5ec5e
Tunghsiao Liu <t@sparanoid.com>
2015-11-08 00:19:50 +0800

    fix: missing scripts

--- _app/assets/_js/user.js
+++ _app/assets/_js/user.js
@@ -1,1 +1,13 @@
 // Custom user scripts
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  var cubes = document.querySelectorAll(".cube-perspective .cube");
+  Array.prototype.forEach.call(cubes, function(el, i){
+    setTimeout(function() {
+      el.className += " hover";
+    }, getRandomArbitrary(1, 5) * 1000);
+  });
+});
